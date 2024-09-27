1ec7b309cf0853e20976a688f18149bf09c94741
Pegasus\kevin <kevinlusignolo@gmail.com>
2024-09-26 20:41:12 -0500

    only allow scoring once per ring

--- scenes/objects/ring.gd
+++ scenes/objects/ring.gd
@@ -1,13 +1,14 @@
 extends StaticBody2D
 
 var has_player_entered_ring = false
-
+var has_score_been_claimed = false
 
 func _on_ring_exit_body_entered(body: Node2D) -> void:
-	if has_player_entered_ring:
-		print("Player passed through the ring")
+	if has_player_entered_ring and not has_score_been_claimed:
+		has_score_been_claimed = true
 		Globals.ring_score += 1
 		has_player_entered_ring = false
+		
 
 
 func _on_ring_entry_body_entered(body: Node2D) -> void:
